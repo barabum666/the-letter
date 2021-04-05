53e26d5e56c833ad9a6c3a45e6ab210d61bce606
barabum666 <65859711+barabum666@users.noreply.github.com>
2021-04-05 15:33:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*-------SLIDER-------*/
+
+new Swiper(".swiper-container", {
+	watchOverflow: true,
+	allowTouchMove: true,
+	loop: true,
+	autoplay: {
+		delay: 7000,
+		disableOnInteraction: false
+	},
+	// Change speed
+	speed: 500,
+});
